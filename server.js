df775df69b82615e4eed71669daca1f512e4bc9f
Diya2675 <156152600+Diya2675@users.noreply.github.com>
2024-06-14 13:43:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const app = express();
 
 
 const HTTP_PORT = process.env.PORT || 8080;
-    
-app.use(express.static("public"));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.redirect('/about');
